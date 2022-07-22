53c4fa9b1fb5a1087be4957e32b3b13fc93ea7ce
IAXRetailer <retailer673@163.com>
2022-07-22 14:35:06 +0800

    format code

--- Library/Web/flaurm/api.py
+++ Library/Web/flaurm/api.py
@@ -6,13 +6,13 @@ class Flaurm():
             "identification":account,
             "password":password
         }
+        self.url=url
         identify=loads(post(url+"/api/token",data=_identify).text)
         self.token=identify["token"]
         self.id=identify["userId"]
-        self.url=url
         self._header={
-            "Authorization":"Token " + self.token+"; userId=%s" % self.id
-        }
+            "Authorization":"Token %s; userId=%s" % (self.token,self.id)
+            }
     def postArticle(self) -> None:
         _data={"data":{"type": "discussions","attributes": {"title": "API test","content": "Hello World"},"relationships":{"tags": {"data": [{"type": "tags","id": "1"}]}}}}
         resp=post(self.url+"/api/discussions",headers=self._header,data=_data)
